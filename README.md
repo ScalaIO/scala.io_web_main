0064d6b7ebef8ee881ff71195f28e9797c7b8261
Jean Helou <jean.helou@gmail.com>
2014-03-31 15:58:24 +0200

    setup vagrant instance for website dev

---
+++ README.md
@@ -0,0 +1,32 @@
+Setting up your dev environment
+==========
+
+* Install vagrant (depends on virtualbox)
+* Clone this repository and checkout branch scalaio-2014
+```
+git clone -b  scalaio-2014 git@github.com:ScalaIO/scala.io_web_main.git
+```
+* cd into the cloned repository
+* run
+````shell
+vagrant up
+vagrant ssh
+````
+
+You now have a VM with all the necessary tools installed to generate the scalaio website
+
+Working on scalaio's website
+==========
+* Ensure your vagrant image is up : `vagrant up`
+* Connect to your image : `vagrant ssh`
+* Go to the source : `cd scalaio`
+* Ensure you have the latest js dependencies: `bower install`
+* Build the website : `grunt build`
+* Start working on the website : `grunt watch` (with livereload and fs monitoring)
+* Access the watched site from the host : 'http://localhost:4090'
+
+
+Deploying the website
+==========
+
+To be decided
---
+++ scalaio-vagrant.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+USER=$1
+
+apt-get update -y
+apt-get install -y curl git
+
+cd /home/$USER
+
+su -l $USER -c 'echo "starting actions as $USER"'
+if [ 0 -eq $? ] ; then
+  su -l $USER -c '\curl -sSL https://get.rvm.io | bash -s stable'
+  su -l $USER -c 'echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config'
+  su -l $USER -c 'git clone https://github.com/ScalaIO/scala.io_web_main.git scalaio'
+  su -l $USER -c 'cd scalaio; git checkout scalaio-2014;cd ..'
+  su -l $USER -c 'rvm install `cat scalaio/.rbenv-version`'
+  su -l $USER -c 'curl https://raw.github.com/creationix/nvm/v0.4.0/install.sh | sh'
+  su -l $USER -c 'nvm install 0.10'
+  su -l $USER -c 'nvm use 0.10'
+  su -l $USER -c 'echo "nvm use 0.10" >> ~/.bash_profile'
+  su -l $USER -c 'cd scalaio;npm install -g bower grunt-cli;cd ..;'
+  su -l $USER -c 'cd scalaio;npm install;cd ..;'
+  su -l $USER -c 'cd scalaio;bower --config.interactive=false install;cd ..;'
+  su -l $USER -c 'cd scalaio;bundle install;cd ..;'
+else
+  echo "failed to act as logged $USER"
+fi
