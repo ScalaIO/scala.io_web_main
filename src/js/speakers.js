7ac48dda2039b78b089d129df740602bc3f33152
Jean Helou <jean.helou@gmail.com>
2014-07-09 14:24:44 +0200

    fix speakers layout

--- src/js/speakers.js
+++ src/js/speakers.js
@@ -1,9 +1,30 @@
+var grouped = function(size, array){
+  var result = []
+  var count = 0;
+  var group = []
+  for (i = 0;i < array.length;i++){
+    if (count < size){
+      group.push(array[i]);
+      count=count+1;
+    }else{
+      result.push(group);
+      group=[array[i]];
+      count=1;
+    }
+  }
+  result.push(group);
+  return result;
+}
 var handleSpeakers=function(data){
   var source = $("#speakers-template").html();
   var template = Handlebars.compile(source);
-  var speakers = data;
-  console.log(speakers);
-  $("#speakers").html(template({speakers:speakers}));
+  var groupedArrays = grouped(5, data);
+  var groups = []
+  for(i=0;i<groupedArrays.length;i++){
+    groups.push({speakers:groupedArrays[i]});
+  }
+  console.dir({groups:groups});
+  $("#speakers").html(template({groups:groups}));
 };
 
 window.onload=function(evt){
