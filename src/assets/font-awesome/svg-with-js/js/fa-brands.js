3c5f830d1af71b3d7221988370c8f2b7ec92c6a8
Fabrice Sznajderman <fabszn@gmail.com>
2019-03-18 11:18:55 +0100

    merge

--- src/assets/font-awesome/svg-with-js/js/fa-brands.js
+++ src/assets/font-awesome/svg-with-js/js/fa-brands.js
@@ -33,7 +33,7 @@ var NAMESPACE_IDENTIFIER = '___FONT_AWESOME___';
 
 var PRODUCTION = function () {
   try {
-    return "production" === 'production';
+    return undefined === 'production';
   } catch (e) {
     return false;
   }
