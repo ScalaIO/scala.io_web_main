ac8a46107585e8d67334d286d580bd7c40708a28
Daniel Lovera <daniel.lovera@dawex.com>
2019-09-22 17:01:35 +0200

    import speakers from papercall

--- src/js/speakers.js
+++ src/js/speakers.js
@@ -21,16 +21,16 @@ scalaio.filter('unsafe', function($sce) {
         return $sce.trustAsHtml(val);
     };
 });
-var scalaioServices = angular.module('scalaioServices', ['ngResource']);
-scalaioServices.factory('Speakers', ['$resource', function($resource) {
-    return $resource(
-        'https://cfp.scala.io/api/conferences/ScalaIOFR2018/speakers/:speakerId', { speakerId: '@speakerId' }, {}
-    );
-}]);
-scalaioServices.factory('Talks', ['$resource', function($resource) {
-    return $resource(
-        'https://cfp.scala.io/api/conferences/ScalaIOFR2018/speakers/:speakerId', { speakerId: '@speakerId' }, {}
-    );
+
+var scalaioServices=angular.module('scalaioServices', ['ngResource']);
+scalaioServices.factory('Speakers', ['$resource', function($resource){
+  return $resource(
+    'json/speakers/:speakerId.json',
+    {speakerId:'@speakerId'},
+    {
+      "query":{method:'GET', url:'json/speakers.json', isArray:true}
+    }
+  );
 }]);
 scalaio.controller('SpeakersCtrl', ['$scope', '$location', 'Speakers', '$rootScope', function($scope, $location, Speakers, $rootScope) {
     $scope.speakerGroups = $rootScope.speakerGroups;
@@ -65,15 +65,18 @@ scalaio.controller('SpeakerCtrl', ['$scope', '$routeParams', '$sce', 'Speakers',
     Speakers.get({ speakerId: $routeParams.speakerId },
         function(data) {
             $scope.speaker = data;
-            $scope.speakerBio = $sce.trustAsHtml('<img class="avatar left" src="' + data.avatarURL + '"></img>' + data.bioAsHtml);
+            $scope.speakerBio = $sce.trustAsHtml('<img class="avatar left" src="' + data.avatar + '"></img>' + data.bio);
         });
+    $scope.getLang = function(tags) {
+        if (tags && tags.includes("🇫🇷")) return "fr";
+        return "en";
+    };
     $scope.hasNoCompany = function(speaker) {
-        console.dir(speaker);
         return !speaker ||  !speaker.company || speaker.company.trim() == "" || speaker.company.trim() == "-" || speaker.company.trim() == " -"
     };
     $scope.hasNoTwitter = function(speaker) {
         return !speaker ||  !speaker.twitter || speaker.twitter.trim() == ""
     };
 }]);
-scalaio.controller('TalkCtrl', function($scope) {});
-scalaio.controller('TalksCtrl', function($scope) {});
+scalaio.controller('SubmissionCtrl', function($scope) {});
+scalaio.controller('SubmissionsCtrl', function($scope) {});
--- src/scripts/papercall.js
+++ src/scripts/papercall.js
@@ -1,16 +1,15 @@
 /*
-* usage:
-*   from 'scripts' directory
-*   npm install request yargs fs
-*   node papercall.js fetch-submissions.js  --token <papercall token>
-*/
-
+ * usage:
+ *   from 'scripts' directory
+ *   npm install request yargs fs
+ *   node papercall.js fetch-submissions.js  --token <papercall token>
+ */
 const request = require('request');
 const yargs = require('yargs');
 const fs = require("fs");
 
 const papercallUrl = 'https://www.papercall.io/api/v1/submissions';
-const destination = '../json/submissions.json'
+const destinationDirectory = '../json'
 
 yargs.command('fetch-submissions', 'Fetch all submissions, filtering fields', (parameters) => {
         parameters.positional('token', {
@@ -47,35 +46,110 @@ function fetchSubmissions(token) {
             return;
         }
 
-        // filter data
-        var jsonData = JSON.parse(body);
-        var filteredData = jsonData.map(function(item) {
-            // remove unwanted fields
-            delete item.state;
-            delete item.confirmed;
-            delete item.created_at;
-            delete item.updated_at;
-            delete item.additional_info;
-            delete item.rating;
-            delete item.trust;
-            delete item.profile.shirt_size;
-            delete item.profile.email;
-            delete item.profile.location;
-            delete item.talk.notes;
-            delete item.cfp_additional_question_answers;
-            item.co_presenter_profiles = item.co_presenter_profiles.map(function(copresenter) {
-                delete copresenter.shirt_size;
-                delete copresenter.email;
-                delete copresenter.location;
-                return copresenter;
+        var submissions = JSON.parse(body);
+
+        // filter submissions
+        var filteredSubmissions = [];
+        submissions.forEach(function(item) {
+            var submission = {}
+            submission['id'] = item.id
+            submission['tags'] = item.tags
+
+            var speaker = {}
+            speaker['name'] = item.profile.name
+            speaker['bio'] = item.profile.bio
+            speaker['twitter'] = item.profile.twitter
+            speaker['company'] = item.profile.company
+            speaker['url'] = item.profile.url
+            speaker['avatar'] = item.profile.avatar
+            submission['profile'] = speaker
+
+            var co_presenter_profiles = []
+            item.co_presenter_profiles.forEach(function(copresenter) {
+                var speaker = {}
+                speaker['name'] = copresenter.name
+                speaker['bio'] = copresenter.bio
+                speaker['twitter'] = copresenter.twitter
+                speaker['company'] = copresenter.company
+                speaker['url'] = copresenter.url
+                speaker['avatar'] = copresenter.avatar
+                co_presenter_profiles.push(speaker)
             });
-            return item;
+            submission['co_presenter_profiles'] = co_presenter_profiles
+
+            var talk = {}
+            talk['title'] = item.talk.title
+            talk['description'] = item.talk.description
+            talk['abstract'] = item.talk.abstract
+            talk['audience_level'] = item.talk.audience_level
+            talk['talk_format'] = item.talk.talk_format
+            submission['talk'] = talk
+
+            // add to submissions
+            filteredSubmissions.push(submission)
         });
 
-        // write filtered data to destination
-        fs.writeFile(destination, JSON.stringify(filteredData, null, 4), (writeError) => {
+        // write filtered submissions data to destination
+        var destination = destinationDirectory + '/submissions.json'
+        fs.writeFile(destination, JSON.stringify(filteredSubmissions, null, 4), (writeError) => {
             if (writeError) console.log(writeErr);
             console.log("Papercall submissions successfully extracted in " + destination);
         });
+
+        // filter speakers
+        var filteredSpeakers = [];
+        filteredSubmissions.forEach(function(item) {
+            var talk = item.talk
+            talk['tags'] = item.tags
+
+            var filteredSpeaker = filteredSpeakers.find(s => s['name'] === item.profile.name);
+            if (filteredSpeaker) {
+                // speaker already added, only add talk
+                filteredSpeaker['talks'].push(talk)
+            } else {
+                // add to speaker with talk if new
+                var speaker = item.profile
+                speaker['talks'] = []
+                speaker['talks'].push(talk)
+                filteredSpeakers.push(speaker)
+            }
+
+            item.co_presenter_profiles.forEach(function(copresenter) {
+                var filteredSpeaker = filteredSpeakers.find(s => s['name'] === copresenter.name);
+                if (filteredSpeaker) {
+                    // speaker already added, only add talk
+                    filteredSpeaker['talks'].push(talk)
+                } else {
+                    // add to speaker with talk if new
+                    var speaker = copresenter
+                    speaker['talks'] = []
+                    speaker['talks'].push(talk)
+                    filteredSpeakers.push(speaker)
+                }
+            });
+        });
+
+        // write each speaker to destination
+        filteredSpeakers.forEach(function(speaker) {
+            var speakerDestination = destinationDirectory + '/speakers/' + speaker.name + '.json'
+            fs.writeFile(speakerDestination, JSON.stringify(speaker, null, 4), (writeError) => {
+                if (writeError) console.log(writeErr);
+                console.log("Papercall " + speaker.name + " speaker successfully extracted in " + speakerDestination);
+            });
+        });
+
+        var filteredSpeakers = filteredSpeakers.map(function(speaker) {
+            filteredSpeaker = {}
+            filteredSpeaker['name'] = speaker.name;
+            filteredSpeaker['avatar'] = speaker.avatar;
+            return filteredSpeaker;
+        });
+
+        var speakerDestination = destinationDirectory + '/speakers.json'
+        fs.writeFile(speakerDestination, JSON.stringify(filteredSpeakers, null, 4), (writeError) => {
+            if (writeError) console.log(writeErr);
+            console.log("Papercall speakers successfully extracted in " + speakerDestination);
+        });
+
     });
 }
